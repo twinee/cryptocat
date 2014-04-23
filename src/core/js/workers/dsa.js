2e514b388b7f02c216768ac70d662165104c28ce
Arlo Breault <arlolra@gmail.com>
2014-04-23 07:40:10 -0700

    Refactor cryptojs includes
     * Adds a build step to the makefile to concatenate the crytojs files.
    
       This makes upgrading straightforward, without the need to add headers
       to each individual file, leaving the source unmodified. It also means
       that one file needs to be included per page.
    
     * Adds pbkdf2.

--- src/core/js/workers/dsa.js
+++ src/core/js/workers/dsa.js
@@ -7,17 +7,7 @@
 
 	// default imports
 	var imports = [
-		'../lib/crypto-js/core.js',
-		'../lib/crypto-js/enc-base64.js',
-		'../lib/crypto-js/cipher-core.js',
-		'../lib/crypto-js/x64-core.js',
-		'../lib/crypto-js/aes.js',
-		'../lib/crypto-js/sha1.js',
-		'../lib/crypto-js/sha256.js',
-		'../lib/crypto-js/sha512.js',
-		'../lib/crypto-js/hmac.js',
-		'../lib/crypto-js/pad-nopadding.js',
-		'../lib/crypto-js/mode-ctr.js',
+		'../lib/crypto-js.js',
 		'../lib/salsa20.js',
 		'../etc/random.js',
 		'../lib/bigint.js',
